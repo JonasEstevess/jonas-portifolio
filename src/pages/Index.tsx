0e638becefd585d37db13a3b08cde86503d9f874
Jonas Esteves <jonasesteves2006@gmail.com>
2023-05-17 10:00:00 -0300

    Add main page components and routing structure

---
+++ src/pages/Index.tsx
@@ -0,0 +1,21 @@
+import { Navbar } from "@/components/portfolio/Navbar";
+import { Hero } from "@/components/portfolio/Hero";
+import { About } from "@/components/portfolio/About";
+import { Projects } from "@/components/portfolio/Projects";
+import { Skills } from "@/components/portfolio/Skills";
+import { Contact } from "@/components/portfolio/Contact";
+
+const Index = () => {
+  return (
+    <main className="min-h-screen bg-background text-foreground">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
+  );
+};
+
+export default Index;
